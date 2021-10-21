afb346a5014fcf7711534c96f583154f7d84f6b5
AlanRHurley3372 <92942957+AlanRHurley3372@users.noreply.github.com>
2021-10-21 14:25:01 -0400

    Update PsuedoDocumentPages.py
    Updated the script and tested to assure function

--- PsuedoDocumentPages.py
+++ PsuedoDocumentPages.py
@@ -1,21 +1,54 @@
+###SCRIPT FOR ITERATING THROUGH FILES AND MOVING OUT FILES LARGER THAN A GIVEN PAGE LIMIT###
+
+
+
 #For iterating over local docs
 import glob
-
 #For using PDF functions
 from PyPDF2 import PdfFileReader
-
 #For progress bar
 from tqdm import tqdm
 
+#Libraries for moving our files
+import os
+import re
 
+#Constant representing max pages we want to work with for now
+TARGET=50
+
+#Array of files to push to side for now
+toStore=[]
 
 #Uses the glob library to iterate over files on local device like one would in a stored array
 #The tqdm in front of the body of the loop here will cause a progress bar output
 #Make sure you adjust the file path to match your machine
 for filepath in tqdm(glob.iglob(r'C:\Users\steel\Documents\NACDL\*.pdf')):
     
-    #Sets and opens current pdf based on the filepath variable in the loop
-    pdf = PdfFileReader(open(filepath,'rb'))
+    #Makes sure pdf reader closes when we are done with it
+    with open(filepath, "rb") as f:
     
-    #Little test to assure page numbers are printed correctly
-    print(pdf.getNumPages())
+        #Sets and opens current pdf based on the filepath variable in the loop
+        pdf = PdfFileReader(f, "rb")
+    
+        #Little test to assure page numbers are printed correctly
+        #print(pdf.getNumPages())
+        pageNum=pdf.getNumPages()
+    
+        #stores files too large in a separate array to help sort later
+        if pageNum>TARGET:
+            
+            #Gets only the file name in the full path for moving purposes
+            filepathslice=(filepath[31:len(filepath)])
+            toStore.append(filepathslice)
+
+#Be sure the files are not open elsewhere on the pc as this will cause error from the program
+for names in toStore:
+    
+    #Should match your normal path
+    toName="C:/Users/steel/Documents/NACDL/" + names
+    
+    #change this to desired target path
+    toReplace="C:/Users/steel/Documents/NACDL/ToUseLater/" + names
+    
+    #Moves each unneeded file to target path
+    os.rename(toName,toReplace)
